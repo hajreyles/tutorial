2501f1cd1439e335f4c3028bd5eeeee0f1161d6a
hacer zeynep <hacrzeyles@gmail.com>
2023-03-16 01:28:06 +0100

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//stateless functional component
+//always return JSX
+//JSX rules___________________________
+// return single element (use React.Fragment for multiple entries)
+// div / section / article or Fragment
+// use camelCase for html attribute (onclick -> onClick)
+// className instead of class (class="" -> className="")
+// close every element (<div> </div>)
+// formatting
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting(){
+  return (
+    <React.Fragment>
+      <h4>This is Hajr and this is my first component.</h4>
+      <h6>And I am learning React.</h6>
+    </React.Fragment>
+  ); 
+}
+
+//For "<React.Fragment> </React.Fragment> " you can use " <> - </> " as a short cut
+
+ReactDom.render(<Greeting/>, document.getElementById("root"));
