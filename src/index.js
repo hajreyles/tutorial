b821eafeb36ac6dc9ca5cf3c3942677bc5437f5c
hacer zeynep <hacrzeyles@gmail.com>
2023-03-17 23:57:55 +0100

    mini book project

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
+function BookList() {
   return ( 
-    <div >
-      <Person />
-      <Message />
-    </div>
+    <section >
+     <Book />
+     <Title />
+     <Author />
+    </section>
 
   );
 }
 
-const Person = () => <h2> Hajr </h2>;
-const Message = () => {
-  return <p> This is my Message </p>;
+const Book = () => {
+  return (
+    <img src="https://m.media-amazon.com/images/I/41+OZ+-AQ6L._SX366_BO1,204,203,200_.jpg" alt="Book_cover" />
+  )
 }
 
-ReactDom.render( < Greeting / > , document.getElementById("root"));
+const Title = () =>
+  <h1>I Didn't Do The Thing Today</h1>;
+
+const Author = () =>
+  <h4>Madeleine Dore</h4>;
+
+ReactDom.render( <BookList /> , document.getElementById("root"));
