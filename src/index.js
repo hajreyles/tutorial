1c82b363ec06ffae1b08de707729feac47ea1a0f
hacer zeynep <hacrzeyles@gmail.com>
2023-03-17 23:42:30 +0100

    Nested component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-//stateless functional component
-//always return JSX
-//JSX rules___________________________
-// return single element (use React.Fragment for multiple entries)
-// div / section / article or Fragment
-// use camelCase for html attribute (onclick -> onClick)
-// className instead of class (class="" -> className="")
-// close every element (<div> </div>)
-// formatting
+function Greeting() {
+  return ( 
+    <div >
+      <Person />
+      <Message />
+    </div>
 
-function Greeting(){
-  return (
-    <React.Fragment>
-      <h4>This is Hajr and this is my first component.</h4>
-      <h6>And I am learning React.</h6>
-    </React.Fragment>
-  ); 
+  );
 }
 
-//For "<React.Fragment> </React.Fragment> " you can use " <> - </> " as a short cut
+const Person = () => <h2> Hajr </h2>;
+const Message = () => {
+  return <p> This is my Message </p>;
+}
 
-ReactDom.render(<Greeting/>, document.getElementById("root"));
+ReactDom.render( < Greeting / > , document.getElementById("root"));
