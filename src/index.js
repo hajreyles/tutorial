38f4e8dac65d828379aeac0e82b6ef15b6be3e84
hacer zeynep <hacrzeyles@gmail.com>
2023-03-18 01:24:13 +0100

    css

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import "./index.css";
+
 function BookList() {
   return ( 
-    <section >
-     <Book />
-     <Title />
-     <Author />
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
     </section>
-
   );
 }
 
 const Book = () => {
   return (
-    <img src="https://m.media-amazon.com/images/I/41+OZ+-AQ6L._SX366_BO1,204,203,200_.jpg" alt="Book_cover" />
-  )
-}
+    <article className="book">
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-const Title = () =>
-  <h1>I Didn't Do The Thing Today</h1>;
+const Image = () => <img src="https://m.media-amazon.com/images/I/41+OZ+-AQ6L._SX366_BO1,204,203,200_.jpg" alt="Book_cover" />
 
-const Author = () =>
-  <h4>Madeleine Dore</h4>;
+const Title = () => <h1>I Didn't Do The Thing Today</h1>;
 
-ReactDom.render( <BookList /> , document.getElementById("root"));
+const Author = () => <h4>Madeleine Dore</h4>;
+
+ReactDom.render( <BookList />, document.getElementById("root"));
